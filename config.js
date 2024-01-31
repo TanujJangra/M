9f802781a5c7d6964b3bb26d0712e249370d2c79
TanujJangra <96954296+TanujJangra@users.noreply.github.com>
2024-01-31 13:09:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  TOKEN: "",
+  ownerID: ["1141031175020888117", ""],
+  botInvite: "",
+  supportServer: "",
+  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  status: 'KHANJAN',
+  commandsDir: './commands',
+  language: "en",
+  embedColor: "00fbff",
+  errorLog: "",
+
+
+  sponsor: {
+    status: true,
+    url: "https://www.youtube.com/@wolfyislive",
+  },
+
+  voteManager: {
+    status: false,
+    api_key: "",
+    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
+    vote_url: "",
+  },
+
+  shardManager: {
+    shardStatus: false
+  },
+
+  playlistSettings: {
+    maxPlaylist: 10,
+    maxMusic: 75,
+  },
+
+  opt: {
+    DJ: {
+      commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle']
+    },
+
+    voiceConfig: {
+      leaveOnFinish: false,
+      leaveOnStop: false,
+      leaveOnEmpty: {
+        status: true,
+        cooldown: 10000000,
+      },
+
+    },
+
+    maxVol: 150,
+
+  }
+}
